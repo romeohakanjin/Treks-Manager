f4246af044d89ed5ade079fe80d87b1bc22dc961
romeohakanjin <hakanjin.romeo@hotmail.fr>
2021-09-09 15:18:42 +0200

    [TreksManager] Initial commit.

---
+++ src/main/java/com/rha/treksmanager/TreksManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rha.treksmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreksManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rha/treksmanager/TreksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rha.treksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
